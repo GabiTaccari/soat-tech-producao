3fa6818d83618850843ff8da3d4933bca427904d
Gabriela Taccari <gabriela.goncalves.taccari@gmail.com>
2026-01-09 20:48:11 -0300

    ci: add PR workflow with coverage gate

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.spec.ts"],
   clearMocks: true,
+  collectCoverage: true,
+  coverageReporters: ["json-summary", "lcov", "text"],
 
   globals: {
     "ts-jest": {
